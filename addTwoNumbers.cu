7f5ee14a1641a3b00a84dd93d41c2ffa64645e0a
Sagnik Roy <sagnik.jal00@gmail.com>
2022-07-14 14:58:40 +0530

    uploaded first cuda code

---
+++ addTwoNumbers.cu
@@ -0,0 +1,32 @@
+// Kylo Ren c2022
+#include<iostream>
+#include<cuda.h>
+
+using namespace std;
+
+__global__ void addInteger(int* a, int* b){
+    a[0] += b[0];
+}
+
+int main(){
+    int a=2, b=3;
+    int *d_a, *d_b;
+
+    cudaMalloc(&d_a, sizeof(int));
+    cudaMalloc(&d_b, sieof(int));
+
+    cudaMemcpy(d_a, &a, sizeof(int), cudaMemcpyHostToDevice);
+    cudaMemcpy(d_b, &b, sizeof(int), cudaMemcpyHostToDevice);
+
+    addInteger<<< 1, 1 >>>(d_a, d_b);
+
+    cudaMemcpy(&a, d_a, sizeof(int), cudaMemcpyDeviceToHost);
+
+    cudaFree(d_a);
+    cudaFree(d_b);
+
+    cout<<"Sum of "<<a<<" and "<<b<<" is : "<< a<<endl;
+
+    return 0;
+}
+
